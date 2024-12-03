b35ded3a5767f2d191812b115d2bd0a88eb4e210
Max Reichenauer <max.reichenauer98@gmail.com>
2024-12-03 11:14:29 +0100

    WIP updated variables file and played around with different schemas

--- client/src/pages/variablesPage/VariablesPage.tsx
+++ client/src/pages/variablesPage/VariablesPage.tsx
@@ -3,7 +3,7 @@ import styles from "./variablesPage.module.css";
 const VariablesPage = () => {
   return (
     <div className={styles.mainContainer}>
-      <div>
+      <div className={styles.heightFitContent}>
         <h2>main colors</h2>
         <div className={styles.colorContainer}>
           <div className={styles.mainColorLight}></div>
@@ -11,7 +11,7 @@ const VariablesPage = () => {
           <div className={styles.mainColorDark}></div>
         </div>
       </div>
-      <div>
+      <div className={styles.heightFitContent}>
         <h2>secondary colors</h2>
         <div className={styles.colorContainer}>
           <div className={styles.secondaryColorLight}></div>
@@ -19,7 +19,7 @@ const VariablesPage = () => {
           <div className={styles.secondaryColorDark}></div>
         </div>
       </div>
-      <div>
+      <div className={styles.heightFitContent}>
         <h2>tertiary colors</h2>
         <div className={styles.colorContainer}>
           <div className={styles.tertiaryColorLight}></div>
@@ -27,7 +27,7 @@ const VariablesPage = () => {
           <div className={styles.tertiaryColorDark}></div>
         </div>
       </div>
-      <div>
+      <div className={styles.heightFitContent}>
         <h2>quaternary colors</h2>
         <div className={styles.colorContainer}>
           <div className={styles.quaternaryColorLight}></div>
@@ -35,13 +35,22 @@ const VariablesPage = () => {
           <div className={styles.quaternaryColorDark}></div>
         </div>
       </div>
-      <div className={styles.fontContainer}>
+      <div className={styles.heightFitContent}>
         <h1 className={styles.heading1}>Heading H1</h1>
         <h2 className={styles.heading2}>Heading H2</h2>
         <h3 className={styles.heading3}>Heading H3</h3>
         <p className={styles.text1}>Text Regular</p>
         <p className={styles.text2}>Text Small</p>
       </div>
+      <div className={styles.buttonContainer}>
+        <h2>Buttons</h2>
+        <button className="primaryButton">Main Button</button>
+        <button className="secondaryButton">Secondary Button</button>
+      </div>
+      <a href="https://fonts.google.com/specimen/Inter" target="_blank">
+        {" "}
+        Fontsiten.com
+      </a>
     </div>
   );
 };
