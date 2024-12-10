83da60ba297d1d560dfa3e7a91b863d92265de70
Max Reichenauer <max.reichenauer98@gmail.com>
2024-12-10 15:39:08 +0100

    Locatiing issues for deployment for getting a story by id

--- client/src/services/serverApi/get/getStory.ts
+++ client/src/services/serverApi/get/getStory.ts
@@ -3,8 +3,14 @@ import { get } from "../instance";
 import { ApiResponse } from "../Models.ts/Types";
 
 const getStory = async (id: string): Promise<StoryType> => {
-  const response = await get<ApiResponse<StoryType>>(`/firebase/stories/${id}`);
-  return response.data.data;
+  try {
+    const response = await get<ApiResponse<StoryType>>(`/firebase/stories/${id}`);
+    console.log(response);
+    return response.data.data;
+  } catch (error) {
+    console.error("Error fetching story:", error);
+    throw new Error("An error occurred while fetching the story");
+  }
 };
 
 export { getStory };
